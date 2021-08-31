083a10b7efb1765e38971ab040a69493ce71a88f
DeepakDK04 <deepak99pmk@gmail.com>
2021-08-31 20:28:12 +0530

    added product, employee models

--- billie/employee/models.py
+++ billie/employee/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Employee(models.Model):
+    '''  Model for Employee Info '''
+    genderchoices = [
+        ("Male", "Male"), ("Female", "Female"), ("Others", "Others")
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=8, unique=True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=6, choices=genderchoices)
+    contactno = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f"{self.code} : {self.user.username}"
--- billie/products/models.py
+++ billie/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    ''' Product Model for Product Info '''
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    code = models.CharField(max_length=8, unique=True)
+    category = models.ManyToManyField('Category')
+
+    def __str__(self) -> str:
+        return f"{self.code} : {self.name}"
+
+
+class Category(models.Model):
+    ''' Model for Category Info '''
+    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=8, unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.code} : {self.name}"
